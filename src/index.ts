088592f99ec3ea176f4d09ce2d2992f8143de6e8
Zoya Pleskacevich <pleskacevich.z@MacBook-Air-Zoya.local>
2021-05-02 18:51:48 +0300

    create a base feature

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+let arr = [1, 2, 3].includes(1)
+
+/* let/const **/
+let period = 50;
+const baseUrl = 'http://example.com';
+
+/* object **/
+let firstName = 'Zoya';
+let account = {
+    firstName,
+    getName() {
+        return this.firstName
+    }
+};
+
+/* spread **/
+let person = {...account};
+let dates = [...[11, 12, 13]];
+
+/* destructor **/
+let {firstName: myName} = person;
+let [firstDate] = dates;
+
+/* template string **/
+function userMessage([start, end]: TemplateStringsArray, {firstName: name}: typeof person): string {
+    return `${start}${name}${end}`
+}
+console.log(userMessage`Good Day, ${person} !!`);
+
+/* for of **/
+for (const date of dates) {
+    console.log(date)
+}
+
+/* arrow **/
+let sum = (a: number, b: number) => a + b;
+
+/* Class **/
+class Point {
+    public x = 10;
+    sum() {
+        return this.x;
+    }
+}
+
+/* Optional chaining **/
+const user: any = {};
+let x = user?.info?.baz();
+
+/* ?? **/
+let admin;
+let a = admin ?? user;
